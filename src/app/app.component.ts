57f1a86b2c23f757806882023e5645a555bf8261
unknown <granadosmiguel832@gmail.com>
2024-03-04 22:08:23 -0600

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-rifas';
+
+  numerosRifa: String[] = [];
+
+  constructor(){
+    this.generarNumerosRifa();
+  }
+
+  generarNumerosRifa() {
+    this.numerosRifa = [];
+    for (let i = 0; i <= 99999; i++) {
+      const numero = ('00000' + i).slice(-5); // Formatea el número para que tenga 5 dígitos
+      this.numerosRifa.push(numero);
+    }
+  }
 }
